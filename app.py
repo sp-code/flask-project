9cfd2044a504577999836bbc1efb7ea2ed14388e
Saurabh Purandare <58353131+sp-code@users.noreply.github.com>
2025-08-15 10:39:11 +0000

    Add data & API route

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote'    
+    }
+]
+
 @app.route("/")
 def hello():
-    return "Hello, Saurabh"
+    return render_template('home.html',jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
